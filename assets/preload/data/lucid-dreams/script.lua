598f3617fd6740300595eeea6e0c2f42082f6ca6
Neon4ek <damirakhm@gmail.com>
2022-01-26 18:46:23 +0500

    Update script.lua

--- assets/preload/data/lucid-dreams/script.lua
+++ assets/preload/data/lucid-dreams/script.lua
@@ -1,23 +1,9 @@
-local allowCountdown = false;
-local playDialogue = false;
+local allowCountdown = false
 function onStartCountdown()
-	-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
-	if not allowCountdown and isStoryMode and not seenCutscene then
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
 		startVideo('coolintro');
 		allowCountdown = true;
-		playDialogue = true;
-		return Function_Stop;
-	elseif playDialogue then
-		setProperty('inCutscene', true);
-		runTimer('startDialogue', 0.8);
-		playDialogue = false;
 		return Function_Stop;
 	end
 	return Function_Continue;
-end
-
-function onTimerCompleted(tag, loops, loopsLeft)
-	if tag == 'startDialogue' then -- Timer completed, play dialogue
-		startDialogue('dialogue', 'talk-time');
-	end
 end
