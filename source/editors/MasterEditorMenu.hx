216e59bf3c75b3f0bc39e8884d6a392debf6fd39
Neon4ek <damirakhm@gmail.com>
2022-03-29 19:55:47 +0500

    Update MasterEditorMenu.hx
    Ummm...

--- source/editors/MasterEditorMenu.hx
+++ source/editors/MasterEditorMenu.hx
@@ -123,6 +123,8 @@ class MasterEditorMenu extends MusicBeatState
 					LoadingState.loadAndSwitchState(new DialogueEditorState(), false);
 				case 'Chart Editor'://felt it would be cool maybe
 					LoadingState.loadAndSwitchState(new ChartingState(), false);
+				case 'Some fuck':
+					openSubState(new GameplayChangersSubstate());
 			}
 			FlxG.sound.music.volume = 0;
 			#if PRELOAD_ALL
