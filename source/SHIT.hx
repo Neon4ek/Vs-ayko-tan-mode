4100693b93d2bf7ed0091943a07be47a6482c6d3
Neon4ek <damirakhm@gmail.com>
2022-03-29 19:35:43 +0500

    Update SHIT.hx
    Well SHIT is still SHIT

--- source/SHIT.hx
+++ source/SHIT.hx
@@ -6,15 +6,27 @@ import flixel.FlxSubState;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import lime.app.Application;
+import flixel.addons.display.FlxBackdrop;
+import flixel.addons.display.FlxGridOverlay;
 
 class SHIT extends MusicBeatState
 {
+	var backdrops:FlxBackdrop;
+
 	override function create()
 	{
 		super.create();
-		var thx:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('pricol1', 'preload'));
-		add(thx);
+		var gay:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('pricol/pricol', 'preload'));
+		add(gay);
 		FlxG.camera.fade(FlxColor.BLACK, 0.8, true);
+
+		backdrops = new FlxBackdrop(Paths.image('pricol/grid'), 0.2, 0.2, true, true);
+		backdrops.alpha = 0.2;
+		backdrops.x -= 35;
+		add(backdrops);
+
+		var gayText:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('pricol/pricolTXT', 'preload'));
+		add(gayText);
 	}
 
 	override function update(elapsed:Float)
@@ -24,5 +36,8 @@ class SHIT extends MusicBeatState
 			FlxG.switchState(new MainMenuState());
 		}
 		super.update(elapsed);
+
+		backdrops.x -= .25*(elapsed/(1/120));
+		backdrops.y += .25*(elapsed/(1/120));
 	}
 }
